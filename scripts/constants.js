dd9fdad3e408c88cf9708f41e64fe9951cbc5ff0
Alex Staravoitau <git@use.fastmail.com>
2024-06-07 14:00:34 +0100

    Update the script
    Update the script, so that it saves SVGs to files instead, and filters them based on the export list.

--- scripts/constants.js
+++ scripts/constants.js
@@ -1,24 +1,14 @@
-const SF_SYMBOLS_VERSION = "4.0";
-
-const WEIGHTS = [
-    // "black",
-    "bold",
-    // "heavy",
-    // "light",
-    // "medium",
-    // "regular",
-    // "semibold",
-    // "thin",
-    // "ultralight",
-];
+const SF_SYMBOLS_VERSION = "5.0";
 
 const PATH_PRECISION = 2;
 
-const FONT_SIZE = 28;
+const FONT_SIZE = 18;
+// Options are: "black", "bold", "heavy", "light", "medium", "regular", "semibold", "thin", "ultralight".
+const WEIGHT = "regular";
 
 module.exports = {
     SF_SYMBOLS_VERSION,
-    WEIGHTS,
+    WEIGHT,
     PATH_PRECISION,
     FONT_SIZE,
 };
--- scripts/makeSymbols.js
+++ scripts/makeSymbols.js
@@ -3,35 +3,36 @@ const path = require("path");
 const opentype = require("opentype.js");
 const SVGPathCommander = require("svg-path-commander");
 const {
-    WEIGHTS,
+    WEIGHT,
     SF_SYMBOLS_VERSION,
     PATH_PRECISION,
     FONT_SIZE,
 } = require("./constants");
 
-// Not sure if this is right
-function writeFile(_path, contents) {
-    fs.mkdirSync(path.dirname(_path), { recursive: true });
-    fs.writeFileSync(_path, contents);
+function createSvgFileContents(pathData, width, height) {
+    return `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<svg width="${width}" height="${height}" viewBox="0 0 ${width} ${height}" xmlns="http://www.w3.org/2000/svg" version="1.1">
+  <path d="${pathData}" fill="currentColor"/>
+</svg>`;
+}
+
+function writeSvgFile(dir, fileName, svgContents) {
+    const svgFilePath = path.join(dir, fileName);
+    fs.mkdirSync(path.dirname(svgFilePath), { recursive: true });
+    fs.writeFileSync(svgFilePath, svgContents);
 }
 
 // Paths
 const PATHS = {
     CHARS: `${__dirname}/../sources/${SF_SYMBOLS_VERSION}/chars.txt`,
     NAMES: `${__dirname}/../sources/${SF_SYMBOLS_VERSION}/names.txt`,
-    OUT: `${__dirname}/../src/symbolSet.json`,
+    EXPORT_NAMES: `${__dirname}/../sources/${SF_SYMBOLS_VERSION}/names.export.txt`,
+    OUT_DIR: `${__dirname}/../output/svg/`, // Directory for SVG files
 };
 
 async function make() {
-    console.log("Loading Fonts...");
-
-    const fonts = await Promise.all(
-        WEIGHTS.map(async (weight) => {
-            const font = await opentype.load(
-                `${__dirname}/../sources/${SF_SYMBOLS_VERSION}/SF-Pro-Text-${weight}.otf`
-            );
-            return font;
-        })
+    const font = await opentype.load(
+        `${__dirname}/../sources/${SF_SYMBOLS_VERSION}/SF-Pro-Text-${WEIGHT}.otf`
     );
 
     console.log("Capturing Paths... This takes a while");
@@ -40,59 +41,46 @@ async function make() {
         .readFileSync(PATHS.CHARS, { encoding: "utf-8" })
         .match(/.{1,2}/g);
 
-    // Load name sequence from text file.
-    const names = fs
-        .readFileSync(PATHS.NAMES, { encoding: "utf8", flag: "r" })
-        .split(/\r?\n/);
+    const namesToExport = fs.readFileSync(PATHS.EXPORT_NAMES, { encoding: "utf8", flag: "r" })
+        .split(/\r?\n/)
 
-    const out = {
-        version: SF_SYMBOLS_VERSION,
-        precision: PATH_PRECISION,
-        fontSize: FONT_SIZE,
-        symbols: names.reduce((acc1, name, namesIndex) => {
-            console.log(`${namesIndex + 1}/${names.length} - ${name}`);
-            return {
-                ...acc1,
-                [name]: WEIGHTS.reduce((acc2, weight, index) => {
-                    const path = fonts[index].getPath(
-                        chars[namesIndex],
-                        0,
-                        0,
-                        FONT_SIZE
-                    );
-                    const bb = path.getBoundingBox();
+    const names = fs.readFileSync(PATHS.NAMES, { encoding: "utf8", flag: "r" })
+        .split(/\r?\n/)
+        .filter(name => namesToExport.includes(name)); // Only process names that are in the names.export file
 
-                    const pathData = path.toPathData(PATH_PRECISION);
+    names.forEach((name, namesIndex) => {
+        console.log(`${namesIndex + 1}/${names.length} - ${name}`);
 
-                    // Move symbol to boundary of future container. This may be unwanted by some because it eliminates baseline consistency between symbols.
-                    const transform = {
-                        translate: [bb.x1 * -1, bb.y1 * -1], // X and Y axis translation
-                        origin: [0, 0],
-                    };
+        const path = font.getPath(
+            chars[namesIndex],
+            0,
+            0,
+            FONT_SIZE
+        );
+        const bb = path.getBoundingBox();
+        const pathData = path.toPathData(PATH_PRECISION);
 
-                    const transformed2DPathString = new SVGPathCommander(
-                        pathData
-                    )
-                        .transform(transform)
-                        .toString();
+        const transform = {
+            translate: [bb.x1 * -1, bb.y1 * -1],
+            origin: [0, 0],
+        };
 
-                    return {
-                        ...acc2,
-                        [weight.toLowerCase()]: {
-                            path: transformed2DPathString,
-                            geometry: {
-                                width: bb.x2 - bb.x1, // Correct for X and Y axis translation
-                                height: bb.y2 - bb.y1, // Correct for X and Y axis translation
-                            },
-                        },
-                    };
-                }, {}),
-            };
-        }, {}),
-    };
+        const transformed2DPathString = new SVGPathCommander(pathData)
+            .transform(transform)
+            .toString();
 
-    console.log("Done!");
-    writeFile(PATHS.OUT, JSON.stringify(out, null, 2));
+        const svgContents = createSvgFileContents(
+            transformed2DPathString,
+            bb.x2 - bb.x1,
+            bb.y2 - bb.y1
+        );
+
+        const svgFileName = `${name}.svg`;
+
+        writeSvgFile(PATHS.OUT_DIR, svgFileName, svgContents);
+    });
+
+    console.log("SVG Generation Complete!");
 }
 
 make();
